62c959eb2e681ffcac7c1a5907ef5180e30e74da
codersunited1459 <codersunited1459@gmail.com>
2025-08-12 16:41:45 +0530

    initial commit

---
+++ userservice/src/main/java/com/user/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.user.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/save")
+    public String userRegistry(){
+        return "User Saved";
+    }
+
+
+}
